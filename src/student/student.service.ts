b54bbff13f366fc266f0c26da7b95ed3a7e2f945
Blessie Madeja <101403984+bmt1101@users.noreply.github.com>
2025-01-04 22:47:04 +0800

    Update student.service.ts

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,17 +1,4 @@
-import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Student } from '../entities/student.entity';
-
-@Injectable()
-export class StudentService {
-  constructor(
-    @InjectRepository(Student)
-    private readonly studentRepository: Repository<Student>,
-  ) {}
-
-  async createStudent(data: Partial<Student>): Promise<Student> {
-    const student = this.studentRepository.create(data);
-    return await this.studentRepository.save(student);
-  }
+async updateStudent(id: number, data: Partial<Student>): Promise<Student> {
+  await this.studentRepository.update(id, data);
+  return await this.studentRepository.findOneBy({ id });
 }
