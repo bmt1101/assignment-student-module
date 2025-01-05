d5ebc6c6b8e648c0791bee8b17efac1784411d65
Blessie Madeja <101403984+bmt1101@users.noreply.github.com>
2025-01-05 19:26:19 +0800

    Update student.controller.ts

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,6 +1,13 @@
-import { Delete, Param } from '@nestjs/common';
+import { Controller, Delete, Param, ParseIntPipe } from '@nestjs/common';
+import { StudentService } from '../students/student.service';
 
-@Delete(':id')
-remove(@Param('id') id: number) {
-  return this.studentService.deleteStudent(id);
+@Controller('students')
+export class StudentController {
+  constructor(private readonly studentService: StudentService) {}
+
+  @Delete(':id')
+  async deleteStudent(@Param('id', ParseIntPipe) id: number): Promise<{ message: string }> {
+    await this.studentService.deleteStudent(id);
+    return { message: `Student with ID ${id} has been deleted` };
+  }
 }
